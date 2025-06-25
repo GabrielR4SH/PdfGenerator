24c11bc2ef7dad216657046ca570b3c66a06b91f
GabrielR4SH <bieelsantana23@hotmail.com>
2025-06-24 22:57:46 -0300

    feat: make index and clear to inputs

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php 
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PDFGeneratorOS\PDF_generator;
+
+function clearTerminal(){
+    if(strtoupper(substr(PHP_OS, 0, 3)) == 'WIN'){
+        system('cls');
+    } else{
+        system('clear');
+    }
+}
